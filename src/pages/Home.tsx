a920a48a95497b41b617c0c19954e8871ce5c677
Ali Selçuk <41209531+aselcuk@users.noreply.github.com>
2021-01-23 01:04:27 +0300

    marvel service integration done (#2)
    Summary:
    - marvel service integration done and character list received from service
    - rendered character list on homepage
    - eslintrc.json & .gitignore fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,52 @@
+import { Avatar, Box, Grid, HStack, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import marvelService from 'src/service/marvel-service';
+
 export default function Home() {
+
+    const [characters, setCharacters] = useState<any>([]);
+
+    useEffect(() => {
+        getCharacters();
+    }, []);
+
+    const getCharacters = async () => {
+        const response = await marvelService.getCharacters();
+
+        setCharacters(response.data.results);
+    };
+
     return (
-        <div>
-            home
-        </div>
+        <Box
+            p='30px'
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+        >
+
+            <Grid templateColumns={['100%', 'repeat(2, 1fr)']} gap={8}>
+                {
+                    characters.map((character) => (
+                        <Link
+                            key={character.id}
+                            to={`/detail/${character.id}`}
+                        >
+                            <HStack
+                                boxShadow='md'
+                                py='2'
+                                px='4'
+                                bg='white'
+                                rounded='md'
+                                spacing='12px'
+                            >
+                                <Avatar size='lg' name={character.name} src={character.thumbnail.path + '.' + character.thumbnail.extension} />
+                                <Text fontSize='xl'>{character.name}</Text>
+                            </HStack>
+                        </Link>
+                    ))
+                }
+            </Grid>
+        </Box>
     );
 }
